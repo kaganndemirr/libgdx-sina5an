64e7607e2331b0451b10c21753c28d87d1082a3a
kaganndemirr <kaganndemirr@gmail.com>
2025-05-15 18:21:10 +0300

    toolchain upgraded to 21

--- core/src/main/java/io/github/test/Main.java
+++ core/src/main/java/io/github/test/Main.java
@@ -3,13 +3,11 @@ package io.github.test;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-import javax.print.DocFlavor;
 import java.util.ArrayList;
 import java.util.List;
 
